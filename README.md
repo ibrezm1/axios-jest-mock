56c0d1e69be3a8ba1680238efedfaea3286f5b81
ibrezm1 <59130871+ibrezm1@users.noreply.github.com>
2022-10-05 22:32:30 +0530

    2 files modified

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # axios-jest-mock
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-2u5wu1)
+
+[Refer](https://vhudyma-blog.eu/3-ways-to-mock-axios-in-jest/)
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // run `node index.js` in the terminal
+// Reference
+// https://vhudyma-blog.eu/3-ways-to-mock-axios-in-jest/
 var { BASE_URL, fetchUsers } = require('./getUsers');
 
 console.log(`Hello Node.js v${process.versions.node}!`);
